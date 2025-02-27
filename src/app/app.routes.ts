55f62621b772b69557955d64b5e6d334547128e2
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2025-02-27 09:50:45 -0300

    fix: fixed layout and page distribution

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+    { path: 'signup-page', loadComponent: () => import('./pages/signup-page/signup-page.component').then(m => m.SignupPageComponent) },
     { path: '**', redirectTo: 'home' }
+
 ];
