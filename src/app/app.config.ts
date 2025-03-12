16eaf8a9f506bb6b14fb4cdcd2dbd0dde9ad97d6
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2025-03-12 11:19:56 -0300

    fix: added httpClient provider and interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { retryErrorInterceptor } from './tools/interceptor/retry-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration()
+  ]
 };
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app-routing.module';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { retryErrorInterceptor } from './app/tools/interceptor/retry-error.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        retryErrorInterceptor
+      ])
+    )
   ]
 }).catch(err => console.error(err));
